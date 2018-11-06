384ff7a6dec4c2cc737c5ac90357b180c3fd9daa
daveodo98 <43785856+daveodo98@users.noreply.github.com>
2018-11-06 11:19:55 -0500

    Add files via upload

--- log.py
+++ log.py
@@ -1,50 +1,7 @@
-from tkinter import *
+from DatabasePush import *
+from MainWindow import *
 
-import firebase_admin as fba
-from firebase_admin import firestore
-from firebase_admin import auth
-import datetime
-import pytz
-import hashlib
-__accountname__=''
-#init database
-cred = fba.credentials.Certificate('./serviceaccountkey.json')
-mainapp = fba.initialize_app(cred)
-db = firestore.client()
-task = db.collection(u'Task')
-users=db.collection(u'User')
-groups=db.collection(u'Group')
 
-def md5(str):
-	#used for password varification
-	hmd5 = hashlib.md5()
-	hmd5.update(str.encode(encoding='utf-8'))
-	return hmd5.hexdigest()
-
-def user_login(email,pwd):
-    #user login system, return a referance of one user if success
-    #global var account name will be reset to this user id
-    if len(list(users.where(u'User_email','==',email).get()))==0:
-        print('Failed: Email incorrect')
-        return None
-    user=list(users.where(u'User_email', '==', email).get())[0]
-    if user.to_dict()['User_pwd']!= md5(pwd):
-        print('Failed: Password incorrect')
-        return None
-    global __accountname__
-    __accountname__=user.id
-    user=users.document(__accountname__)
-    user.update({'Is_login':True,'Last_login':datetime.datetime.utcnow()})
-    return user
-
-def user_add(email,username, pwd):
-	#add a new user to database system, passward encrypted.
-	#will reject creation if username already exict(return a string to indicate that)
-    if len(list(users.where(u'User_email','==',email).get()))>=1:
-        print("Email already exists")
-        return None
-    user_dict={u'User_email':email,u'User_pwd':md5(pwd),u'User_name':username,u'Last_login':None,u'Is_login':False}
-    return users.add(user_dict)[1]
 
 gui = Tk()
 username = 'jia'
@@ -67,11 +24,13 @@ xx = Checkbutton(gui, text="remember me").grid(row=4, column=1)
 x = 0
 y = 0
 
+def log(gui):
+    if user_login(email.get(), pwd.get()) != False:
+        gui = CallMainWindow(user_login(email.get(), pwd.get()))
 
 
 
-
-c1 = Button(gui, background="#6666ee", text="LOGIN", command=lambda: user_login(email.get(),pwd.get())).grid(row=6, column=1)
-g = Button(gui,background="#6666ee", text="Create Account", command = lambda: user_add(email.get(),usr.get(),pwd.get())).grid(row=6,column = 4)
+c1 = Button(gui, background="#6666ee", text="LOGIN", command=lambda: log(gui)).grid(row=6, column=1)
+g = Button(gui,background="#6666ee", text="Create Account", command = lambda: user_add(email.get(),pwd.get(),usr.get())).grid(row=6,column = 4)
 
 gui.mainloop()
