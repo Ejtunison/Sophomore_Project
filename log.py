eeb30e94e3a1a108d16d6c623a2be6e51bf05ef3
daveodo98 <43785856+daveodo98@users.noreply.github.com>
2018-11-08 14:27:55 -0500

    Add files via upload

--- log.py
+++ log.py
@@ -15,12 +15,9 @@ email = StringVar()
 
 # pre = Label(gui ,text="LOG IN", background="#6666ee").grid(row=0,column = 0)
 h = Label(gui,text = "Email").grid(row=1,column=0)
-a = Label(gui, text="username").grid(row=2, column=0)
 b = Label(gui, text="password").grid(row=3, column=0)
 i = Entry(gui, textvariable = email).grid(row=1,column=1)
-e = Entry(gui, textvariable=usr).grid(row=2, column=1)
 f = Entry(gui, show="*", textvariable=pwd).grid(row=3, column=1)
-xx = Checkbutton(gui, text="remember me").grid(row=4, column=1)
 x = 0
 y = 0
 
@@ -29,10 +26,37 @@ def log():
         global gui
         gui.destroy()
         CallMainWindow(user_login(email.get(), pwd.get()))
+    else:
+        global gui
+        a = Label(gui,Text = "Login Failed. Please Try Again").grid(row = 9, column = 0)
 
 
 
-c1 = Button(gui, background="#6666ee", text="LOGIN", command=lambda: log()).grid(row=6, column=1)
-g = Button(gui,background="#6666ee", text="Create Account", command = lambda: user_add(email.get(),pwd.get(),usr.get())).grid(row=6,column = 4)
+def add():
+    add = Tk()
+    h = Label(add, text="Email").grid(row=1, column=0)
+    a = Label(add, text="username").grid(row=2, column=0)
+    b = Label(add, text="password").grid(row=3, column=0)
+    i = Entry(add, textvariable=email).grid(row=1, column=1)
+    e = Entry(add, textvariable=usr).grid(row=2, column=1)
+    f = Entry(add, show="*", textvariable=pwd).grid(row=3, column=1)
+    button = Button(add, background = "#6666ee",text = "Create Account", command = lambda: algo(add)). grid(row = 4, column = 0)
+    add.mainloop()
+
+def algo(frame):
+    if user_add(email,pwd,usr) != False:
+        frame.destroy()
+        global gui
+        a = Label(gui,text="Account Created. Please Login").grid(row=9,column=0)
+    else:
+        frame.destroy()
+        global gui
+        a = Label(gui,text = "Account Already Exists. Please Login").grid(row = 9, column = 0)
+
+
+c1 = Button(gui, background="#6666ee", text="Login", command=lambda: log()).grid(row=6, column=1)
+g = Button(gui,background="#6666ee", text="Click Here to Create Account", command = lambda: add()).grid(row=8,column = 1)
+
+
 
 gui.mainloop()
