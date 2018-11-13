405ba56ebed12e1cd13b1f863a1b8791c13d2519
Ejtunison <43186393+Ejtunison@users.noreply.github.com>
2018-11-13 11:38:48 -0500

    v2.0

--- New_Task_Window.py
+++ New_Task_Window.py
@@ -4,13 +4,13 @@ def Call_Window():
     window.title("New Task")
     window.configure(background="#f6e0f4")
 
-    task = Label(window, text="Title:", bg="#f6e0f4", fg="#8b01a5", font="Times 12")
+    task = Label(window, text="Title:", bg="#f6e0f4", fg="#8b01a5", font="Courier 12")
     titleEntry = Entry(window, font="Courier", fg="#8b01a5", bg="white")
-    description = Label(window, text="Description:", bg="#f6e0f4", fg="#8b01a5", font="Times 12")
+    description = Label(window, text="Description:", bg="#f6e0f4", fg="#8b01a5", font="Courier 12")
     descriptionEntry = Text(window, height=7, width=25, font="Courier", fg="#8b01a5", bg="white")
-    deadline = Label(window, text="Dealine:", bg="#f6e0f4", fg="#8b01a5", font="Times 12")
+    deadline = Label(window, text="Deadline:", bg="#f6e0f4", fg="#8b01a5", font="Courier 12")
     deadlineEntry = Entry(window, font="Courier", fg="#8b01a5", bg="white")
-    user = Label(window, text="User:", bg="#f6e0f4", fg="#8b01a5", font="Times 12")
+    user = Label(window, text="User:", bg="#f6e0f4", fg="#8b01a5", font="Courier 12")
     userEntry = Entry(window, font="Courier", fg="#8b01a5", bg="white")
 
     task.grid(row=1, column=1, sticky=E, padx=3, pady=3)
--- Project_GUI.py
+++ Project_GUI.py
@@ -3,11 +3,48 @@ from New_Task_Window import Call_Window
 
 root = Tk()
 root.title("Task Your Life")
-root.geometry("200x200")
+root.geometry("600x600")
 
-newBoardButton = Button(root, text="New Task Board", bg="#4ef4f9",fg="black", font="Times 12 bold")
-newBoardButton.grid(row=4, column=4)
-addTaskButton = Button(root, text="Add New Task", bg="#70fd5a",fg="black", font="Times 12 bold", command=Call_Window)
-addTaskButton.grid(row=6, column=4)
+#Frames
+topFrame = Frame(root, bg="#cfd4d3")
+topFrame.pack(anchor=N, fill=X, expand=TRUE)
+topFrame.grid_columnconfigure(0, weight=1)
+topFrame.grid_rowconfigure(0, weight=1)
+
+bottomFrame = Frame(root, bg="white")
+bottomFrame.pack(fill=BOTH, expand=TRUE)
+bottomFrame.pack_propagate(FALSE)
+bottomFrame.grid_columnconfigure(0, weight=1)
+bottomFrame.grid_rowconfigure(0, weight=1)
+
+boardButtonFrame = Frame(topFrame)
+boardButtonFrame.grid(sticky=NW, padx=10, pady=10)
+
+inProgressFrame = Frame(bottomFrame, bg="white", highlightbackground="#97f4df", highlightcolor="#97f4df",
+                        highlightthickness=3, width=500, height=700, bd=0)
+inProgressFrame.grid(row=1, column=0)
+inProgressFrame.grid_propagate(FALSE)
+
+#Buttons
+newBoardButton = Button(boardButtonFrame, text="New Task Board", bg="#4ef4f9", fg="black", font="Courier 12 bold")
+newBoardButton.grid(row=0, column=0)
+
+logoutButton = Button(topFrame, text="Log out", bg="#6beec9", fg="black", font="Courier 12 bold")
+logoutButton.grid(row=0, column=1, sticky=NW, padx=10, pady=10)
+
+addTaskButton = Button(bottomFrame, text="Add New Task", bg="#70fd5a",fg="black", font="Courier 12 bold",
+                       command=Call_Window)
+addTaskButton.grid(row=0, column=0, padx=10, pady=10, sticky=NW)
+
+#Labels
+complete = Label(bottomFrame, text="Complete", fg="black", bg="white", font="Courier")
+complete.grid(row=0, column=0, sticky=N, padx=10, pady=10)
+
+inProgress = Label(bottomFrame, text="In Progress", fg="black", bg="white", font="Courier")
+inProgress.grid(row=0, column=1, sticky=NW, padx=10, pady=10)
+
+#Entries
+
+
 
 root.mainloop()
